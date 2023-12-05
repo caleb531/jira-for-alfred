70f0747ec274306ef19eb3007ed1f91c7e9f8c8e
Caleb Evans <caleb@calebevans.me>
2023-12-04 19:58:18 -0800

    Expose issue type as variable for each result

--- jfa/list_issues.py
+++ jfa/list_issues.py
@@ -5,36 +5,38 @@ import sys
 
 import jfa.core as core
 
-issue_types = [
-    {"slug": "bug", "keyword": "10303", "icon": "bug.svg"},
-    {"slug": "subtask", "keyword": "10316", "icon": "subtask.svg"},
-    {"slug": "task", "keyword": "10318", "icon": "task.svg"},
-    {"slug": "scenario", "keyword": "10320", "icon": "scenario.svg"},
-    {"slug": "test", "keyword": "10624", "icon": "test.png"},
-    {"slug": "epic", "keyword": "epic", "icon": "epic.svg"},
-    {"slug": "story", "keyword": "story", "icon": "story.svg"},
+issue_type_defs = [
+    {"type": "bug", "keyword": "10303", "icon_path": "jfa/icons/bug.svg"},
+    {"type": "subtask", "keyword": "10316", "icon_path": "jfa/icons/subtask.svg"},
+    {"type": "task", "keyword": "10318", "icon_path": "jfa/icons/task.svg"},
+    {"type": "scenario", "keyword": "10320", "icon_path": "jfa/icons/scenario.svg"},
+    {"type": "test", "keyword": "10624", "icon_path": "jfa/icons/test.png"},
+    {"type": "epic", "keyword": "epic", "icon_path": "jfa/icons/epic.svg"},
+    {"type": "story", "keyword": "story", "icon_path": "jfa/icons/story.svg"},
 ]
 
 
-# Retrieve the local file path corresponding to the type of the current issue
-def get_issue_icon_path(issue):
-    for issue_type in issue_types:
-        if issue_type["keyword"] in issue["img"]:
-            return f"jfa/icons/{issue_type['icon']}"
-    return "icon.png"
+# Retrieve an object representing the type of this particular issue
+def get_issue_type_def(issue):
+    for issue_type_def in issue_type_defs:
+        if issue_type_def["keyword"] in issue["img"]:
+            return issue_type_def
+    return {"type": "unknown", "keyword": None, "icon": "icon.png"}
 
 
 # Convert the given dictionary representation of a Jira issue to a Alfred
 # feedback result dictionary
 def get_result_from_issue(issue):
+    issue_type_def = get_issue_type_def(issue)
     return {
         "title": issue["summaryText"],
         "subtitle": f"{issue['key']} (view in Jira)",
         "arg": issue["id"],
-        "icon": {"path": get_issue_icon_path(issue)},
+        "icon": {"path": issue_type_def["icon_path"]},
         "variables": {
             "issue_id": issue["id"],
             "issue_key": issue["key"],
+            "issue_type": issue_type_def["type"],
             "issue_summary": issue["summaryText"],
             "issue_url": "{base_url}/browse/{issue_id}".format(
                 base_url=core.ACCOUNT_BASE_URL, issue_id=issue["key"]
